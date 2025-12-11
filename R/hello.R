9d5372c3eccecddd4e5eacfa4019f08f566bb019
pedersebastian <peder.s.martinsen@gmail.com>
2025-12-11 18:26:08 +0100

    setup

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,9 @@
 hello <- function() {
   print("Hello, world!")
 }
+usethis::use_git()
+usethis::use_github()
+usethis::use_tidy_github()
+usethis::use_tidy_github_actions()
+usethis::use_tidy_github_labels()
+usethis::use_pkgdown_github_pages()
