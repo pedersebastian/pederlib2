9c370e73309ce1024e7b9db92d95a4feca3027a7
pedersebastian <peder.s.martinsen@gmail.com>
2025-12-11 18:46:22 +0100

    fix

---
+++ R/startup.R
@@ -0,0 +1,16 @@
+
+
+#' Start up
+#'
+#' @param type with type
+#' @param ... add more packs
+#'
+#' @returns list of attached packages
+#' @export
+#'
+#' @examples
+#'
+#' startup()
+startup <- function(type, ...) {
+  return(NULL)
+}
