2558655f5fa079192adeeaa3ae0dcd62993ab097
pedersebastian <peder.s.martinsen@gmail.com>
2025-12-13 09:58:18 +0100

    rm covarage

--- R/startup.R
+++ R/startup.R
@@ -2,8 +2,14 @@
 
 #' Start up
 #'
-#' @param type with type
-#' @param ... add more packs
+#' @param ... packs to add
+#' @param quiet description
+#' @description
+#' type = 1 - tidyverse + readxl
+#' type = 2 - tidymodels core pkg TODO
+#' type = 3 - tidymodel core + ext TODO
+#' type = 4 - web TODO
+#'
 #'
 #' @returns list of attached packages
 #' @export
@@ -11,19 +17,83 @@
 #' @examples
 #'
 #' startup()
-startup <- function(type = 1, ...) {
-  type_1 <- c("tidyverse", "readxl")
-
-  if (type == 1) pks <- type_1
-
-  map(pks, \(x) suppressPackageStartupMessages(require(x, character.only = TRUE)))
-  pks
+startup <- function(..., quiet = FALSE) {
 
   ggplot2::update_geom_defaults("rect", list(fill = "#1d3557", alpha = 0.9))
   ggplot2::update_geom_defaults("point", list(color = "#1d3557", alpha = 0.9))
 
-  # if (!quiet) {
-  #   cli::cli_alert_success("Geom defaults updated ✌️")
-  # }
-  #suppressMessages(suppressWarnings(require(i, character.only = TRUE)))
+  type_1_info <- NULL
+  type_2_info <- NULL
+
+  if (rlang::dots_n(...) == 0) {
+    dots <- 1
+
+  }
+  else {
+    dots <- rlang::dots_list(...)
+    dots <- unlist(dots)
+  }
+
+
+  type_2 <- c(tidymodels::tidymodels_packages())
+
+
+  if (1 %in% dots) {
+    map(c("tidyverse", "readxl"), \(x) attach_pkg(x))
+
+    type_1_info <- c(
+      cli::rule(center = cli::col_blue(" * Tidyverse: * ")),
+      map(tidyverse::tidyverse_packages(), \(x) print_pkg(x, TRUE)),
+      map("readxl", \(x) print_pkg(x, FALSE))
+    )
+
+  }
+
+
+  ##________________##
+  msg <- c(
+    cli::rule(cli::style_bold("Attaching packs:")),
+    "",
+    type_1_info %||% "",
+    "",
+    "",
+    "",
+    ""
+  )
+
+
+  ##________________##
+
+
+  if (!quiet) {
+    message(paste(msg, collapse = "\n"))
+    cli::cli(cli::cli_alert_success("Geom defaults updated"))
+  }
+
+  invisible()
 }
+
+attach_pkg <- function(pkg) {
+  suppressWarnings(suppressPackageStartupMessages(library(pkg, character.only = TRUE, warn.conflicts = FALSE)))
+}
+
+print_pkg <- function(pkg, indent = FALSE) {
+  version <- as.character(utils::packageVersion(pkg))
+
+  out <-  paste0(
+    cli::col_green(cli::symbol$tick),
+    " ",
+    cli::col_blue(format(pkg)),
+    " ",
+    cli::ansi_align(version, 10)
+
+  )
+  if (indent) {
+    return(paste0(cli::style_bold(cli::symbol$em_dash)," ",
+                  out))
+  }
+  out
+
+}
+
+
