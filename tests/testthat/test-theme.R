3965ba60a4428673d42920edc5eea41dffe5e9a7
pedersebastian <pedersebastian@users.noreply.github.com>
2026-01-06 16:53:14 +0000

    Style code (GHA)

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -2,7 +2,6 @@ library(ggplot2)
 library(vdiffr)
 
 
-
 plot1 <-
   ggplot(mtcars, aes(mpg, disp)) +
   geom_point() +
@@ -16,16 +15,13 @@ plot2 <-
   theme_pedr()
 
 
-
 test_that("simple theme", {
-
   vdiffr::expect_doppelganger(
     "Simple plot with Times New Roman as font",
     plot1
-    )
+  )
   vdiffr::expect_doppelganger(
     "Simple plot with default as font",
     plot2
   )
-
 })
